97f228ddf87c9288cf0d9830527722bfb89c1cc5
Ayush Bhagwan Fanse <87874012+AyushFanse@users.noreply.github.com>
2022-01-30 19:13:50 +0530

    Capstone Project Backend

--- connection.js
+++ connection.js
@@ -6,10 +6,11 @@ const mongoose = require('mongoose');
 
 exports.connect = () => {
     try{
-        mongoose.connect('mongodb+srv://Recipe_CMS:Recipe_CMS@recipe-cms.3i7cy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
+        mongoose.connect('mongodb+srv://RECIPES:RECIPES@recipes.laned.mongodb.net/RECIPES?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
     } catch(err) {
         alert('Error connecting to Server')
         process.exit();
     }
 }
 
+// `//mongodb+srv://Recipe_CMS:Recipe_CMS@recipe-cms.3i7cy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
